24826b99897cf34a8288fcd876b426c72c6cba2c
João Guimarães <joaogmlima@gmail.com>
2021-05-14 00:40:04 -0300

    segundo commit

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,8 +7,44 @@
 module Handler.Home where
 
 import Import
+import Text.Lucius
+import Text.Julius
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                PAGINA 1
+                
+        |]
+
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                PAGINA 2
+        |]        
+    
+getPage3R :: Handler Html
+getPage3R = do
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                PAGINA 3
+        |]
+    
+-- O ideal eh ter apenas chamadas a templates.
+-- css_bootstrap_css => css/bootstrap.css
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do 
+        -- estatico
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(juliusFile "templates/home.julius")
+        -- dinamico
+        toWidgetHead $(luciusFile "templates/home.lucius")
+        $(whamletFile "templates/home.hamlet")
